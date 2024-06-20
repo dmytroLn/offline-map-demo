8867b21e42359dc3b8b5524c1eb0a12ce3fe06cd
Dmytro <dmitiyoleynik@gmail.com>
2024-06-20 13:51:28 +0300

    feature: fix map styles

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,8 @@ import "@ionic/react/css/palettes/dark.system.css";
 /* Theme variables */
 import "./theme/variables.css";
 
+import "mapbox-gl/dist/mapbox-gl.css";
+
 setupIonicReact();
 import { useEffect } from "react";
 
